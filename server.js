f85cb16ff37097aa348d0087c48be0bf88590766
Subin Chaliyath <subinchaliyath@gmail.com>
2020-05-28 21:34:35 +0530

    routing for login and register

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 
-app.get('/',(req,res)=>{
-    res.render('index.ejs')
-})
+app.set('view-engine', 'ejs')
 
+app.get('/', (req, res) => {
+    res.render('index.ejs', { name: "subin" })
+})
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
 app.listen(3000)
